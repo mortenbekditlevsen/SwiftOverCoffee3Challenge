121878338817e93a4703731960616fbdccf7ae9f
Morten Bek Ditlevsen <morten@ka-ching.dk>
2020-03-01 13:52:03 +0100

    Initial Commit

---
+++ SwiftOverCoffee3Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftOverCoffee3Challenge
+//
+//  Created by Morten Bek Ditlevsen on 01/03/2020.
+//  Copyright © 2020 Ka-ching. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
